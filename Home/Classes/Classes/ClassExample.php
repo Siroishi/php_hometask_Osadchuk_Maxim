1542638b9108872d9f40a3803623af2b69793d8c
ETI-activestudents <eti.activestudents@gmail.com>
2023-03-27 16:44:42 +0300

    examples

--- Home/Classes/Classes/ClassExample.php
+++ Home/Classes/Classes/ClassExample.php
@@ -4,5 +4,10 @@ namespace Phpcourse\Myproject\Classes\Classes;
 
 class ClassExample
 {
-
+    public static string $helloInformation =
+        "Check nginx.conf for the root directory of the project.<br>
+        for example, your project is in D:\Folder\Project<br>
+        then the root directory in nginx.conf is \"D:\Folder\Project\public\"<br>
+        so you can access the project by typing http://127.0.0.1<br>
+        or http://localhost<br>";
 }
--- Home/public/index.php
+++ Home/public/index.php
@@ -1,5 +1,13 @@
 <?php
-    require_once '../../vendor/autoload.php';
-    use Phpcourse\Myproject\Classes\MyMainClass;
-    $myMainClass = new MyMainClass();
+    require_once '../../vendor/autoload.php'; // Autoload files using Composer autoload
+    use Phpcourse\Myproject\Classes\MyMainClass; // Use the namespace of the class you want to use
+    $myMainClass = new MyMainClass(); // Create an instance of the class
+
+    // example of using the class constant
+    echo "<br><br>Example of using the class constant:<br>";
     echo $myMainClass::MY_CONST;
+
+    // example of using the class static property
+    echo "<br><br>Example of using the class static property:<br>";
+    use Phpcourse\Myproject\Classes\Classes\ClassExample;
+    echo ClassExample::$helloInformation;
